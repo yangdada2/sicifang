091df5779830a2b4693188180ea356ad9e37d4c7
yangdada2 <429347238@qq.com>
2018-12-06 13:33:33 +0800

    新建十次方工程标签项目tensquare_base

--- tensquare_common/src/main/java/entity/Result.java
+++ tensquare_common/src/main/java/entity/Result.java
@@ -15,6 +15,12 @@ public class Result {
         this.data = data;
     }
 
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
     public Result() {
     }
 
